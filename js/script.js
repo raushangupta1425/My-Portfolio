bba376b1bd1a5338b7b2826347ef8a517628c9d7
Raushan Gupta <126952814+raushangupta1425@users.noreply.github.com>
2024-04-02 12:26:15 +0000

    add skills

--- js/script.js
+++ js/script.js
@@ -7,8 +7,14 @@ $(document).ready(function(){
         $("#html").animate({"width":"70%"},1000,function(){
             $("#css").animate({"width":"80%"},1000,function(){
                 $("#js").animate({"width":"60%"},1000,function(){
-                    $("#jquery").animate({"width":"50%"},1000,function(){
-                        $("#bootstrap").animate({"width":"50%"},1000);
+                    $("#jquery").animate({"width":"80%"},1000,function(){
+                        $("#bootstrap").animate({"width":"70%"},1000,function(){
+                            $("#ajax").animate({"width":"80%"},1000,function(){
+                                $("#sql").animate({"width":"90%"},1000,function(){
+                                    $("#php").animate({"width":"70%"},1000,);
+                                });
+                            });
+                        });
                     });
                 });
             });
